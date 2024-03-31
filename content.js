5e67d5d33844adf2caa71f5cdf09146028b7d389
Chase Manning <chase@manning.dev>
2024-03-31 19:00:24 +0100

    :rocket: remove footer and banner

--- content.js
+++ content.js
@@ -1,5 +1,9 @@
 const INTERVAL = 100;
 
+const hornAlert = () => {
+  const horn = getElementsByClassName("horn");
+};
+
 let checkElementInterval = setInterval(async () => {
   // Getting elements
   const pageFrame = document.getElementsByClassName("pageFrameView");
@@ -8,9 +12,18 @@ let checkElementInterval = setInterval(async () => {
   if (!pageSidebar) return;
   const columns = document.getElementsByClassName("pageFrameView-column");
   if (!columns) return;
+  const campPageBanner = document.getElementsByClassName("campPage-banner");
+  if (!campPageBanner) return;
+  const footer = document.getElementsByClassName("pageFrameView-footer");
 
   // We've got everything we need
   clearInterval(checkElementInterval);
+
+  // Setting interveral for regular triggers
+  setInterval(() => {
+    // Remove the banner
+    campPageBanner[0].parentElement.removeChild(campPageBanner[0]);
+  }, INTERVAL);
 
   // Hiding the sidebar and background
   pageFrame[0].style.gridTemplateColumns = "1fr";
@@ -20,4 +33,7 @@ let checkElementInterval = setInterval(async () => {
   for (let i = columns.length - 1; i >= 0; i--) {
     columns[i].parentElement.removeChild(columns[i]);
   }
+
+  // Remove the footer
+  footer[0].parentElement.removeChild(footer[0]);
 }, INTERVAL);
